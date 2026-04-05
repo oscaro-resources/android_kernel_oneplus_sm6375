0055d587c27f140ba7c4082fa5f847f748a9bbaa
Lup Gabriel <gwolf2u.website@gmail.com>
2026-04-05 11:35:24 +0300

    drivers: ksu: change guard for ksu_vfs_read_hook and ksu_input_hook
    ld.lld: error: undefined symbol: ksu_vfs_read_hook
    >>> referenced by read_write.c:604 (../../../../../../kernel/oneplus/sm8350/fs/read_write.c:604)
    >>>               vmlinux.o:(__arm64_sys_read)
    >>> referenced by read_write.c:604 (../../../../../../kernel/oneplus/sm8350/fs/read_write.c:604)
    >>>               vmlinux.o:(__arm64_sys_read)
    
    ld.lld: error: undefined symbol: ksu_input_hook
    >>> referenced by input.c:457 (../../../../../../kernel/oneplus/sm8350/drivers/input/input.c:457)
    >>>               vmlinux.o:(input_event)
    >>> referenced by input.c:457 (../../../../../../kernel/oneplus/sm8350/drivers/input/input.c:457)
    >>>               vmlinux.o:(input_event)

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -447,7 +447,7 @@ static void input_handle_event(struct input_dev *dev,
  * axis, etc.
  */
 
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 extern bool ksu_input_hook __read_mostly;
 extern __attribute__((cold)) int ksu_handle_input_handle_event(
 			unsigned int *type, unsigned int *code, int *value);
@@ -458,7 +458,7 @@ void input_event(struct input_dev *dev,
 {
 	unsigned long flags;
 
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_MANUAL_HOOK
 	if (unlikely(ksu_input_hook))
 		ksu_handle_input_handle_event(&type, &code, &value);
 #endif
