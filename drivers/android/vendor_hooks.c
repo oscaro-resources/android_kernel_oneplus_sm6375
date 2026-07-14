bf5f6b293f350f0fb5903ac4df05cfa2a7c7cf10
rmp22 <195054967+rmp22@users.noreply.github.com>
2026-07-14 18:27:46 +0800

    [vendor_hooks] adding iolimit vh

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -22,6 +22,7 @@
 #include <trace/hooks/sysrqcrash.h>
 #include <trace/hooks/cgroup.h>
 #include <trace/hooks/sys.h>
+#include <trace/hooks/fs.h>
 #include <trace/hooks/gpu.h>
 
 /*
@@ -88,4 +89,5 @@ EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_map_util_freq);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_em_pd_energy);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_cgroup_set_task);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_syscall_prctl_finished);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_iolimit_rw);
 EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_gpu_context_create);
